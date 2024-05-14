7f083ea956393a3f47b844503eb954e224f7e98c
jeraldjoyson21 <jeraldjoyson21@gmail.com>
2024-05-14 16:02:19 +0000

    initial commit which has main screen and home screen

---
+++ lib/doctor_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class DoctorScreen extends StatelessWidget {
+  List imgs = [
+    "images/doctor1.jpg",
+    "images/doctor2.jpg",
+    "images/doctor3.jpg",
+    "images/doctor4.jpg",
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
