a5fcf0d1566912af37edc46071cc792c8d4dc961
jeraldjoyson21 <jeraldjoyson21@gmail.com>
2024-05-16 18:00:30 +0000

    message screen is editing

--- lib/message_screen.dart
+++ lib/message_screen.dart
@@ -1,10 +1,150 @@
 import 'package:flutter/material.dart';
 
 class MessageScreen extends StatelessWidget {
-  const MessageScreen({super.key});
-
+  List<AssetImage> images = [
+    AssetImage("images/doctor1.jpg"),
+    AssetImage("images/doctor2.jpg"),
+    AssetImage("images/doctor4.jpg"),
+    AssetImage("images/doctor1.jpg"),
+    AssetImage("images/doctor2.jpg"),
+  ];
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return SingleChildScrollView(
+      child: Padding(
+        padding: EdgeInsets.only(top: 30),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 15),
+              child: Text(
+                "Messages",
+                style: TextStyle(
+                  fontSize: 25,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            Container(
+              margin: EdgeInsets.only(
+                top: 5,
+                bottom: 20,
+                left: 15,
+                right: 15,
+              ),
+              width: MediaQuery.of(context).size.width,
+              height: 55,
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black12,
+                    spreadRadius: 3,
+                    blurRadius: 6,
+                  ),
+                ],
+              ),
+              child: Padding(
+                padding: const EdgeInsets.only(left: 15),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintText: "Search",
+                    hintStyle: TextStyle(
+                      color: Colors.black.withOpacity(0.5),
+                      fontSize: 20,
+                    ),
+                    suffixIcon: Icon(
+                      Icons.search,
+                      size: 25,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(left: 15),
+              child: Text(
+                "Active Now",
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.w500,
+                  color: Colors.black.withOpacity(0.7),
+                ),
+              ),
+            ),
+            SizedBox(height: 15),
+            Container(
+              height: 80,
+              child: ListView.builder(
+                shrinkWrap: true,
+                scrollDirection: Axis.horizontal,
+                itemCount: images.length,
+                itemBuilder: (context, index) {
+                  return InkWell(
+                    onTap: () {},
+                    child: Container(
+                      margin: EdgeInsets.symmetric(vertical: 5, horizontal: 8),
+                      height: 80,
+                      width: 80,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        shape: BoxShape.circle,
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black12,
+                            blurRadius: 4,
+                            spreadRadius: 2,
+                          ),
+                        ],
+                      ),
+                      child: Center(
+                        child: Stack(
+                          children: [
+                            CircleAvatar(
+                              radius: 40,
+                              backgroundImage: images[index],
+                            ),
+                            Positioned(
+                              top: 2,
+                              right: 3,
+                              child: Container(
+                                width: 15, // Adjust size as needed
+                                height: 15, // Adjust size as needed
+                                decoration: BoxDecoration(
+                                  shape: BoxShape.circle,
+                                  color: Colors.green,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+
+                    ),
+                  );
+                },
+              ),
+            ),
+            SizedBox(height: 15),
+            Padding(
+              padding: EdgeInsets.only(left: 15),
+              child: Text(
+                "Recent Chat",
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.w500,
+                  color: Colors.black.withOpacity(0.7),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
